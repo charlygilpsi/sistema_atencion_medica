be0a30b8bef0696e4a726ec9456c6778daac0799
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-31 20:49:35 +0200

    añadido todo

--- model/system.py
+++ model/system.py
@@ -1,3 +1,5 @@
 # TODO class System
 # TODO attributes: employees: list[Employee], technicians: list[Technicians], incidences: list[Incidence], tickets: list[Ticket]
 # TODO methods: check_technicians_availability(), create_ticket()
+
+# TODO class Equipment ??
