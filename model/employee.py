945132a039496317e6931c6429f0ce1b19ab8176
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-03-31 19:58:17 +0200

    class employee

--- model/employee.py
+++ model/employee.py
@@ -1,3 +1,23 @@
-# TODO class Employee
-# TODO attributes: id_employee: int, employee_code: int
-# TODO methods: report_incidence(description: str, priority: int, employee_code: int)
+class Employee:
+    def __init__(self, id_employee: int, employee_code: int):
+        self._id_employee = id_employee
+        self._employee_code = employee_code
+        
+        
+    @property
+    def id_employee(self) -> int:
+        return self._id_employee
+    
+    
+    @property
+    def employee_code(self) -> int:
+        return self._employee_code
+    
+    
+    @employee_code.setter
+    def employee_code(self, employee_code: int) -> None:
+        self._employee_code = employee_code
+
+    
+    def report_incidence(self, description: str, priority: int) -> tuple[str, int, int]:
+        return [description, priority, self.employee_code]
