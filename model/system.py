85496895cbeb6d78ba59298c33684d264c02bf48
psi.carlosgil@gmail.com <carlosgdaries@gmail.com>
2026-04-01 20:41:10 +0200

    return changes

--- model/system.py
+++ model/system.py
@@ -73,16 +73,20 @@ class System:
         return max_id + 1
     
     
-    def create_incidence(self, employee_code: int, equipment: Equipment, employee_description: str, priority: int) -> None:
+    def create_incidence(self, employee_code: int, equipment: Equipment, employee_description: str, priority: int) -> Incidence:
         id = self.auto_increment_id(self.incidences)
         incidence = Incidence(id, employee_code, equipment, employee_description, priority)
         self._incidences.append(incidence)
+        
+        return incidence
     
     
-    def create_ticket(self, incidence: Incidence, technician: Technician | None) -> None:
+    def create_ticket(self, incidence: Incidence, technician: Technician | None) -> Ticket:
         id = self.auto_increment_id(self.tickets)
         ticket = Ticket(id, incidence, technician)
         self._tickets.append(ticket)
+        
+        return ticket
     
     
     def check_for_an_available_technician(self) -> Technician | None:
